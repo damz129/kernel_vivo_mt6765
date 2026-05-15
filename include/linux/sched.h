8ac89125938e04a3242826e06605e556135d30df
damz129 <hoanggiakieuhuu@gmail.com>
2026-05-15 15:13:31 +0700

    Update sched.h

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1299,13 +1299,8 @@ struct task_struct {
 
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
-#ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	u64				android_kabi_reserved3;
-	u64				android_kabi_reserved4;
-#else
 	ANDROID_KABI_RESERVE(3);
 	ANDROID_KABI_RESERVE(4);
-#endif
 	ANDROID_KABI_RESERVE(5);
 	ANDROID_KABI_RESERVE(6);
 	ANDROID_KABI_RESERVE(7);
