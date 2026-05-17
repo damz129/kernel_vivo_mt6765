34e73431041c69207f31067d2cef0075f2da9251
damz129 <hoanggiakieuhuu@gmail.com>
2026-05-17 15:49:22 +0000

    codespace temp

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,9 +3,22 @@
 #git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b brillo-m9-release
 #git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 -b android11-mainline-release clang_tool
 
-PATH=`pwd`/clang_tool/clang-r383902/bin:`pwd`/arm-linux-androideabi-4.9/bin:$PATH
+export PATH="/workspaces/kernel_vivo_mt6765/clang-aosp/bin:/workspaces/kernel_vivo_mt6765/gcc-64/bin:/workspaces/kernel_vivo_mt6765/gcc-32/bin:$PATH"
 rm -rf out
 mkdir -p out
 
-make -j32 O=out ARCH=arm CROSS_COMPILE=arm-linux-androideabi- CC=clang CLANG_TRIPLE=arm-linux-gnueabi- LD=ld.lld k65v1_32_bsp_2g_ago_defconfig
-make -j32 O=out ARCH=arm CROSS_COMPILE=arm-linux-androideabi- CC=clang CLANG_TRIPLE=arm-linux-gnueabi- LD=ld.lld
+make O=out ARCH=arm64 \
+    CC=clang \
+    CLANG_TRIPLE=aarch64-linux-gnu- \
+    CROSS_COMPILE=aarch64-linux-android- \
+    CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+    PD2036F_EX_defconfig && \
+make -j$(nproc --all) O=out ARCH=arm64 \
+    CC=clang \
+    CLANG_TRIPLE=aarch64-linux-gnu- \
+    CROSS_COMPILE=aarch64-linux-android- \
+    CROSS_COMPILE_ARM32=arm-linux-androideabi- \
+    Image
+cd out/arch/arm64/boot/
+cp Image kernel
+gzip -n -9 kernel
